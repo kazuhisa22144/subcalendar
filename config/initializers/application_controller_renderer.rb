895dc36d55a310961893adb74410de3d374023f6
kazuhisa22144 <mkazuhisa22144>
2019-09-08 15:58:14 +0000

    [header top users create]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8f2a394e13a3137ed1f21ba468b966ec3ce0f94cf9f5ac1010fe6c5fbebbaef4f442ffe495dd296350e64bd2865f2adb043397638027a6f77fd674217f76a987'

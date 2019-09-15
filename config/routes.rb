348bb2161e12a2bdeb2ddeadcb17ef29d2e4b148
kazuhisa22144 <mkazuhisa22144>
2019-09-15 09:00:27 +0000

    [post comment2]

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  root to: 'calendar#index'
-  get 'calendar/index'
-  get 'events', to: 'event#show'
-  post 'events/create', to: 'event#create'
-  root 'users#top'
+
   resources :users, only: [:index,:show, :edit, :update]
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   resource :post_comments, only: [:create, :destroy]
   
 end
+  root 'users#top'
+  root to: 'calendar#index'
+  get 'calendar/index'
+  get 'events', to: 'event#show'
+  post 'events/create', to: 'event#create'
 end

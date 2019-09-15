bed1261936451c9d7d00d0a78ba86d4bc49d0e32
kazuhisa22144 <mkazuhisa22144>
2019-09-15 08:41:00 +0000

    [post comment add]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_115313) do
+ActiveRecord::Schema.define(version: 2019_09_15_082123) do
 
   create_table "events", force: :cascade do |t|
     t.integer "user_id"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2019_09_14_115313) do
     t.datetime "start"
     t.datetime "end"
     t.string "allDay"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

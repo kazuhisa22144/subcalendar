fcdde0bd2c1f7f68d7e16645ad6af943ef94c5d7
kazuhisa22144 <mkazuhisa22144>
2019-09-18 11:30:38 +0000

    [user:leave,destroy]

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'post_images#top'
   root to: 'calendar#index'
+  get '/users/leave' => 'users#leave'
   get '/users/search' => 'users#search'
   get 'calendar/index'
   get 'events', to: 'event#show'
@@ -9,7 +10,7 @@ Rails.application.routes.draw do
   post 'events/create', to: 'event#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:index,:show, :edit, :update]
+  resources :users, only: [:index,:show, :edit, :update, :destroy]
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   resource :post_comments, only: [:create, :destroy]
   resource :likes, only: [:create, :destroy]
